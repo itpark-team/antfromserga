53984b682783ba5c2cf8607a750db1764f6f7be2
itpark <a.m.smyslov@yandex.ru>
2023-03-31 22:29:39 +0300

    1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import {Menu} from "antd";
+import {Route, Routes, useNavigate} from "react-router-dom";
+import {HomeOutlined, UnorderedListOutlined} from "@ant-design/icons"
+import React from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    const navigate = useNavigate()
+    return (
+        <div style={{display: "flex", flexDirection: "row"}}>
+            <Menu
+                OnClick={({key}) => {
+                    if (key === "notFind") {
+                    } else {
+                        navigate(key);
+                    }
+                }
+                }
+                items={[
+                    {
+                        label: "Home",
+                        key: "/",
+                        icon: <HomeOutlined/>
+                    },
+                    {
+                        label: "Films",
+                        key: "/films",
+                        icon: <UnorderedListOutlined/>
+                    },
+                    {
+                        label: "Profile",
+                        key: "/profile", danger: true
+                    },
+                    {label: "Style_films", key: "/styleFilms",},
+
+
+                ]}>
+            </Menu>
+            <Content/>
+        </div>
+    );
+}
+
+function Content() {
+    return <div>
+        <Routes>
+            <Route path="/" element={<div>Home</div>}></Route>
+            <Route path="/films" element={<div>films</div>}></Route>
+            <Route path="/profile" element={<div>Profile</div>}></Route>
+            <Route path="/styleFilms" element={<div>Style Films</div>}></Route>
+            <Route path="/not_find" element={<div>not page</div>}></Route>
+        </Routes>
     </div>
-  );
 }
 
 export default App;
